0e2602a2a79fa08ab204a599f0b00ed20b97dfa9
Jazib Hassan <jazibulhassan95@gmail.com>
2023-08-12 12:08:15 +0100

    working on Backstage passes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -26,15 +26,13 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Aged Brie")
-                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (isNotSpecialItem(i)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-
+                    items[i].quality = items[i].quality - 1;
                 }
-            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality < 50){
+            }
+
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality < 50){
                 items[i].quality = items[i].quality + 1;
                 if (items[i].sellIn < 11 && items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -60,5 +58,11 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isNotSpecialItem(int i) {
+        return !items[i].name.equals("Aged Brie")
+            && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
+            && !items[i].name.equals("Sulfuras, Hand of Ragnaros");
     }
 }
