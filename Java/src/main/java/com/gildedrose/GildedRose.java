a05051c1c8dad36d60aa1590df6526d99492c98b
Jazib Hassan <jazibulhassan95@gmail.com>
2023-08-12 12:04:59 +0100

    working on Backstage passes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -35,23 +35,13 @@ class GildedRose {
 
                 }
             } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality < 50){
-
                 items[i].quality = items[i].quality + 1;
-
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
+                if (items[i].sellIn < 11 && items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
                 }
-
+                if (items[i].sellIn < 6 && items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
             }
 
 
