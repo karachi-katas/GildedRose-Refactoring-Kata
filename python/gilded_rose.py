3bf4eb6fe8f57fd5b417f5bea84b4d8c4ad73135
manassiddiqui <manassiddiquifolio3.com>
2023-08-12 16:02:42 +0500

    extracted more functions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,20 +9,22 @@ class GildedRose(object):
         for item in self.items:
             if self.is_not_Backstage_pass_and_not_aged_brie(item.name):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if self.is_not_Sulfuras_Hand_of_Ragnaros(item.name):
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if self.is_Backstage_pass(item.name,item.sell_in,item.quality):
+                    if self.is_Backstage_pass_less_than_11_days(item.name,item.sell_in,item.quality):
                         item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+                    if self.is_Backstage_pass_less_than_6_days(item.name,item.sell_in,item.quality):
+                        item.quality = item.quality + 1
+            if self.is_not_Sulfuras_Hand_of_Ragnaros(item.name):
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if self.is_not_Sulfuras_Hand_of_Ragnaros(item.name):
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
@@ -30,13 +32,17 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
-    def is_Backstage_pass(self,item_name, item_sell_in ,item_quality):
+    def is_Backstage_pass_less_than_11_days(self,item_name, item_sell_in ,item_quality):
         return item_sell_in < 11 and item_quality < 50 and item_name =="Backstage passes to a TAFKAL80ETC concert"
 
-                            
+    def is_Backstage_pass_less_than_6_days(self,item_name, item_sell_in ,item_quality):
+        return item_sell_in < 6 and item_quality < 50 and item_name =="Backstage passes to a TAFKAL80ETC concert"
 
     def is_not_Backstage_pass_and_not_aged_brie(self,item_name):
         return item_name != "Aged Brie" and item_name != "Backstage passes to a TAFKAL80ETC concert"
+
+    def is_not_Sulfuras_Hand_of_Ragnaros(self,item_name):
+        return item_name!= "Sulfuras, Hand of Ragnaros"
 
 
 class Item:
