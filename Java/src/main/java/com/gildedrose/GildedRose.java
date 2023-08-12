eb6e53c2a87bd95c6ba8e1bb155dfaa7512ccf2a
Jazib Hassan <jazibulhassan95@gmail.com>
2023-08-12 11:47:57 +0100

    refactored

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,48 +10,56 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
             if (items[i].name.equals("Aged Brie")) {
-                if (items[i].sellIn < 0 && items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
+                applyAgedBrieRules(i);
             }
 
             if (isNotSpecialItem(i)) {
-                if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
-                }
-
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                applyNotSpecialItemRules(i);
             }
 
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality < 50) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                    if (items[i].sellIn < 11 && items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                    if (items[i].sellIn < 6 && items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-
-                if (items[i].sellIn < 0) {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                applyBackstagePassRules(i);
             }
+        }
+    }
+
+    private void applyBackstagePassRules(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+            if (items[i].sellIn < 11 && items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
+            }
+            if (items[i].sellIn < 6 && items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
+            }
+        }
+
+        if (items[i].sellIn < 0) {
+            items[i].quality = items[i].quality - items[i].quality;
+        }
+    }
+
+    private void applyNotSpecialItemRules(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
+
+        if (items[i].sellIn < 0 && items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
+    }
+
+    private void applyAgedBrieRules(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].sellIn < 0 && items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+        }
+
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
         }
     }
 
